cc7717cbce91a9b4321267dc5c2183fd37fc265c
konasiva7 <sivakona@chainscore.finance>
2025-06-25 17:33:34 +0530

    added ring_vrf interfaces

---
+++ dot_ring/vrf/ring/ring_vrf.py
@@ -0,0 +1,47 @@
+#create the ring vrf class and put in the signature generation and verification logics
+
+from typing import List
+
+from dot_ring.ring_proof.gotos import*
+
+class RingVrf:
+
+    @staticmethod
+    def generate_bls_signature(secret_t,producer_key, keys: List[Any]):
+        """
+            get the all the data needed and
+            return the signature as an output"
+            """
+        return generate_bls_signature(secret_t, producer_key, keys)
+
+    @staticmethod
+    def construct_ring_root(keys: List[Any]):
+        """
+        get the data needed and construct the rng root
+        """
+        return construct_ring_root(keys)
+
+    @staticmethod
+    def verify_signature(message:bytes,ring_root:bytes, ring_signature:bytes):
+        """
+        get the bls signature, other params if needed and verify it
+        """
+        return verify_signature(message,ring_root, ring_signature)
+
+    @staticmethod
+    def ring_vrf_proof(alpha:bytes, add:bytes, blinding_factor:bytes, secret_key, producer_key:bytes, keys:List[Any] ):
+        """get the args u want and generate the
+        ring_vrf_proof (pedersen vrf proof + ring_proof ) \
+        which of length 784 bytes"""
+        return ring_vrf_proof(alpha, add, blinding_factor, secret_key,producer_key, keys)
+
+    @staticmethod
+    def ring_vrf_proof_verify(ad_data:bytes, ring_root:bytes, signature:bytes, message=b"")->bool:
+        """get the c, r, signature, m and verify the signature"""
+        #verfify the signature (pedersen_proof+ring_proof)
+        return ring_vrf_proof_verify(ad_data, ring_root, signature)
+
+    @staticmethod
+    def pedersen_proof_to_hash(pedersen_proof:bytes):
+        """get the pedersen proof alone and return the 32 bytes hash"""
+        return pedersen_proof_to_hash(pedersen_proof)
