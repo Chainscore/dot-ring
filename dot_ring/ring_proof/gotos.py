e8c04b8670e508912c559202fe5e924f74e31a3e
konasiva7 <sivakona@chainscore.finance>
2025-06-25 17:30:51 +0530

    added goto funcs for ring_vrf

--- dot_ring/ring_proof/gotos.py
+++ dot_ring/ring_proof/gotos.py
@@ -1,44 +1,56 @@
 
-from typing import List
-from jam.ring_vrf.ring_proof.short_weierstrass.banders import TwistedEdwardCurve
-from jam.types.base import ByteArray32
-from jam.types.protocol.crypto import BandersnatchPublic
-from jam.ring_vrf.ring_proof.columns.columns import PublicColumnBuilder as PC
-from jam.ring_vrf.ring_proof.pcs.load_powers import g1_points, g2_points
-from jam.ring_vrf.ring_proof.transcript.phases import phase1_alphas
-from jam.ring_vrf.ring_proof.transcript.transcript import Transcript
-# from jam.ring_vrf.ring_proof.short_weierstrass.curve import ShortWeierstrassCurve as sw
-from jam.ring_vrf.ring_proof.constants import Blinding_Base, S_PRIME, OMEGA_2048, SeedPoint
-from jam.ring_vrf.ring_proof.columns.columns import WitnessColumnBuilder, PublicColumnBuilder
-from jam.ring_vrf.ring_proof.constraints.constraints import RingConstraintBuilder
-from jam.ring_vrf.ring_proof.helpers import Helpers as H
+from typing import List, Any
+from dot_ring.ring_proof.curve.bandersnatch import TwistedEdwardCurve
+# from dot_ring.types.base import ByteArray32
+# from dot_ring.types.protocol.crypto import BandersnatchPublic
+from dot_ring.ring_proof.columns.columns import PublicColumnBuilder as PC
+from dot_ring.ring_proof.pcs.load_powers import g1_points, g2_points
+from dot_ring.ring_proof.transcript.phases import phase1_alphas
+from dot_ring.ring_proof.transcript.transcript import Transcript
+# from dot_ring.ring_proof.short_weierstrass.curve import ShortWeierstrassCurve as sw
+from dot_ring.ring_proof.constants import Blinding_Base, S_PRIME, OMEGA_2048, SeedPoint
+from dot_ring.ring_proof.columns.columns import WitnessColumnBuilder, PublicColumnBuilder
+from dot_ring.ring_proof.constraints.constraints import RingConstraintBuilder
+from dot_ring.ring_proof.helpers import Helpers as H, Helpers
 from py_ecc.optimized_bls12_381 import normalize as nm
-from jam.ring_vrf.ring_proof.constraints.aggregation import aggregate_constraints
-from jam.ring_vrf.ring_proof.proof.quotient_poly import QuotientPoly
-from jam.ring_vrf.ring_proof.proof.linearization_poly import LAggPoly
-from jam.ring_vrf.ring_proof.proof.aggregation_poly_and_proof_gtn import AggPoly
-from jam.ring_vrf.ring_proof.verfiey import Verify
-from jam.ring_vrf.ring_proof.constants import D_512 as D
-from jam.ring_vrf.curve.specs.bandersnatch import (
+from dot_ring.ring_proof.constraints.aggregation import aggregate_constraints
+from dot_ring.ring_proof.proof.quotient_poly import QuotientPoly
+from dot_ring.ring_proof.proof.linearization_poly import LAggPoly
+from dot_ring.ring_proof.proof.aggregation_poly import AggPoly
+from dot_ring.ring_proof.verify import Verify
+from dot_ring.ring_proof.constants import D_512 as D
+from dot_ring.curve.specs.bandersnatch import (
     Bandersnatch_TE_Curve,
     BandersnatchPoint,
 )
-from jam.ring_vrf.pedersen.pedersen import PedersenVRF
+from dot_ring.vrf.pedersen.s_pedersen import PedersenVRF
 
 
-def generate_bls_signature(secret_t,producer_key, keys: List[BandersnatchPublic]):
+# from dot_ring.vrf.pedersen.pedersen import PedersenVRF
+
+
+# def generate_bls_signature(secret_t,producer_key, keys: List[BandersnatchPublic]):
+def generate_bls_signature(secret_t:bytes|str,producer_key:bytes|str, keys: List[Any]):
     """
     get the all the data needed and
     return the signature as an output
     """
     # signature = func() #make the call or logic u want
     # return signature
-    producer_key_point=BandersnatchPoint.string_to_point(bytes(producer_key))
-    producer_key_pt= producer_key_point.x, producer_key_point.y
+    if not isinstance(producer_key, bytes):
+        producer_key_point = BandersnatchPoint.string_to_point(bytes(producer_key))
+    else:
+        producer_key_point = BandersnatchPoint.string_to_point(producer_key)
+
+    secret_t=Helpers.l_endian_2_int(secret_t)
+    producer_key_pt= (producer_key_point.x, producer_key_point.y)
     keys_as_bs_points = []
 
     for key in keys:
-        point = BandersnatchPoint.string_to_point(bytes(key))  # or take key[2:] by skipping '0x'
+        if isinstance(key, bytes):
+            point = BandersnatchPoint.string_to_point(key)
+        else:
+            point = BandersnatchPoint.string_to_point(bytes(key))  # or take key[2:] by skipping '0x'
         keys_as_bs_points.append((point.x, point.y))
 
     ring_root = PC()  # ring_root builder
@@ -87,8 +99,8 @@ def generate_bls_signature(secret_t,producer_key, keys: List[BandersnatchPublic]
     cf_vs, proof_ptr, proof_bs = obj.construct_proof()
     return proof_bs #bytess string
 
-
-def construct_ring_root(keys: List[BandersnatchPublic]):
+# def construct_ring_root(keys: List[BandersnatchPublic]):
+def construct_ring_root(keys: List[Any]):
     """
     get the data needed and construct the rng root
     """
@@ -108,12 +120,15 @@ def construct_ring_root(keys: List[BandersnatchPublic]):
     return fxd_col_cs
 
 
-def verify_signature(message, ring_root, proof):
+def verify_signature(message:bytes|str, ring_root:bytes|str, proof:bytes|str):
     """
     get the bls signature, other params if needed and verify it
     """
     # is_valid=func() # make the func call or logic u want
     # return is_valid
+    if not isinstance(proof, bytes):
+        proof=bytes.fromhex(proof)
+
     proof_ptr = [H.bls_g1_decompress(proof[:48]),
                  H.bls_g1_decompress(proof[48 * 1: 48 * 2]),
                  H.bls_g1_decompress(proof[48 * 2: 48 * 3]),
@@ -130,6 +145,8 @@ def verify_signature(message, ring_root, proof):
                  H.bls_g1_decompress(proof[48 * 4 + (7 * 32) + 48 + 32:-48]),
                  H.bls_g1_decompress(proof[-48:])]
 
+    if not isinstance(ring_root, bytes):
+        ring_root=bytes.fromhex(ring_root)
 
     rltn_to_proove =BandersnatchPoint.string_to_point(message) #relation to proove
     rltn=(rltn_to_proove.x, rltn_to_proove.y)
@@ -146,39 +163,43 @@ def verify_signature(message, ring_root, proof):
     return valid.is_signtaure_valid()
 
 
-def ring_vrf_proof(alpha, add, blinding_factor, producer_key, keys:List[BandersnatchPublic]):
+# def ring_vrf_proof(alpha, add, blinding_factor, producer_key, keys:List[BandersnatchPublic]):
+def ring_vrf_proof(alpha:bytes|str, add:bytes|str, blinding_factor:bytes|str, secret_key:bytes|str, producer_key:bytes|str, keys:List[Any]):
     """get the args u want and generate the
     ring_vrf_proof (pedersen vrf proof + ring_proof ) \
     which of length 784 bytes"""
+    if not isinstance(alpha, bytes):
+        alpha=bytes.fromhex(alpha)
+
+    if not isinstance(add, bytes):
+        add= bytes.fromhex(add)
+    if not isinstance(add, bytes):
+        add = bytes.fromhex(add)
+    if not isinstance(blinding_factor, bytes):
+        blinding_factor = bytes.fromhex(blinding_factor)
+    if not isinstance(add, bytes):
+        secret_key = bytes.fromhex(secret_key)
 
     #pedersen_proof=get the pedersen proof
     vrf = PedersenVRF(Bandersnatch_TE_Curve, BandersnatchPoint)
-    blinding_f_int = (
-            int.from_bytes(blinding_factor)
-            % Bandersnatch_TE_Curve.ORDER
-    )
-    secret_scalar = (
-            int.from_bytes(producer_key)
-            % Bandersnatch_TE_Curve.ORDER
-    )
-    output_point, proof = vrf.prove(
-        alpha,
-        secret_scalar,
-        add,
-        blinding_f_int,
-    )
-    pedersen_proof=output_point.point_to_string().hex()+proof[0].point_to_string().hex() +proof[1].point_to_string().hex()+proof[2].point_to_string().hex() +H.to_bytes(proof[3]) +H.to_bytes(proof[4])
-
+    pedersen_proof = vrf.prove(alpha,secret_key,add,blinding_factor)
     #ring_proof= get the ring_signature
-    ring_proof= generate_bls_signature(blinding_f_int,producer_key, keys)
+    ring_proof= generate_bls_signature(blinding_factor,producer_key, keys)
+    if not isinstance(ring_proof, bytes):
+        ring_proof=bytes.fromhex(ring_proof)
     rvrf_proof= pedersen_proof +ring_proof
     return rvrf_proof
 
 
-def pedersen_proof_to_hash(pedersen_proof):
+def pedersen_proof_to_hash(pedersen_proof:bytes|str):
     """get the pedersen proof alone and return the 32 bytes hash"""
+    if not isinstance(pedersen_proof, bytes):
+        pedersen_proof=bytes.fromhex(pedersen_proof)
+
     if int.from_bytes(pedersen_proof[:32], 'little') == 0:
-        return ByteArray32(pedersen_proof[:32])
+        # return ByteArray32(pedersen_proof[:32])
+        return pedersen_proof[:32]
+
     vrf = PedersenVRF(Bandersnatch_TE_Curve, BandersnatchPoint)
     # extract the fist 32 bytes as it's the gamma
     gamma= BandersnatchPoint.string_to_point(pedersen_proof[:32])
@@ -188,26 +209,24 @@ def pedersen_proof_to_hash(pedersen_proof):
 
 #pedersen+ring_proof verification
 
-def ring_vrf_proof_verify(context, ring_root, proof, alpha=b""): #context(add), ring_root, signature, message(alpha)
+def ring_vrf_proof_verify(context:bytes|str, ring_root:bytes|str, proof:bytes|str, alpha:bytes|str=b""): #context(add), ring_root, signature, message(alpha)
+    if not isinstance(context,bytes):
+        context=bytes.fromhex(context)
+    if not isinstance(ring_root, bytes):
+        ring_root=bytes.fromhex(ring_root)
+    if not isinstance(proof, bytes):
+        proof=bytes.fromhex(proof)
+    if not isinstance(alpha, bytes):
+        alpha=bytes.fromhex(alpha)
 
     pedersen_proof= proof[:192]
     vrf = PedersenVRF(Bandersnatch_TE_Curve, BandersnatchPoint)
     #get the input point
     input_point = BandersnatchPoint.encode_to_curve(alpha)
-    output_point=BandersnatchPoint.string_to_point(pedersen_proof[32*0:32*1]) #O.p
-    rel_to_proove = BandersnatchPoint.string_to_point(pedersen_proof[32*1:32*2]) #Y'
-    R=BandersnatchPoint.string_to_point(pedersen_proof[32*2:32*3]) #R
-    Ok=BandersnatchPoint.string_to_point(pedersen_proof[32*3:32*4])#Ok
-    S=H.bytes_to_int(pedersen_proof[32*4:32*5])
-    Sb=H.bytes_to_int(pedersen_proof[32*5:32*6])
-
-    proof_tup=(rel_to_proove,R, Ok, S, Sb)
 
     #is pedersen proof valid
-    p_proof_valid= vrf.verify(
-        input_point, context , output_point, proof_tup
-    )
-
+    p_proof_valid= vrf.verify(input_point, context , pedersen_proof)
+    rel_to_proove=BandersnatchPoint.string_to_point(pedersen_proof[32:64])
     #Extract and verify the Ring proof
     ring_proof=proof[192:]
     proof_ptr = [H.bls_g1_decompress(ring_proof[:48]),
