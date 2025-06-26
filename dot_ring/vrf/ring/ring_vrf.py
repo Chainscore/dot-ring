ca1f87a187c5474ecb56ec127cb2a23115f4e0b2
konasiva7 <sivakona@chainscore.finance>
2025-06-26 13:16:03 +0530

    added strict types

--- dot_ring/vrf/ring/ring_vrf.py
+++ dot_ring/vrf/ring/ring_vrf.py
@@ -7,7 +7,7 @@ from dot_ring.ring_proof.gotos import*
 class RingVrf:
 
     @staticmethod
-    def generate_bls_signature(secret_t,producer_key, keys: List[Any]):
+    def generate_bls_signature(secret_t:bytes|str,producer_key:bytes|str, keys: List[Any])->bytes:
         """
             get the all the data needed and
             return the signature as an output"
@@ -15,33 +15,33 @@ class RingVrf:
         return generate_bls_signature(secret_t, producer_key, keys)
 
     @staticmethod
-    def construct_ring_root(keys: List[Any]):
+    def construct_ring_root(keys: List[Any])->bytes:
         """
         get the data needed and construct the rng root
         """
         return construct_ring_root(keys)
 
     @staticmethod
-    def verify_signature(message:bytes,ring_root:bytes, ring_signature:bytes):
+    def verify_signature(message:bytes|str,ring_root:bytes|str, ring_signature:bytes|str)->bool:
         """
         get the bls signature, other params if needed and verify it
         """
         return verify_signature(message,ring_root, ring_signature)
 
     @staticmethod
-    def ring_vrf_proof(alpha:bytes, add:bytes, blinding_factor:bytes, secret_key, producer_key:bytes, keys:List[Any] ):
+    def ring_vrf_proof(alpha:bytes|str, add:bytes|str, blinding_factor:bytes|str, secret_key:bytes|str, producer_key:bytes|str, keys:List[Any])->bytes:
         """get the args u want and generate the
         ring_vrf_proof (pedersen vrf proof + ring_proof ) \
         which of length 784 bytes"""
         return ring_vrf_proof(alpha, add, blinding_factor, secret_key,producer_key, keys)
 
     @staticmethod
-    def ring_vrf_proof_verify(ad_data:bytes, ring_root:bytes, signature:bytes, message=b"")->bool:
+    def ring_vrf_proof_verify(ad_data:bytes|str, ring_root:bytes|str, signature:bytes|str, message:bytes|str=b"")->bool:
         """get the c, r, signature, m and verify the signature"""
         #verfify the signature (pedersen_proof+ring_proof)
         return ring_vrf_proof_verify(ad_data, ring_root, signature)
 
     @staticmethod
-    def pedersen_proof_to_hash(pedersen_proof:bytes):
+    def pedersen_proof_to_hash(pedersen_proof:bytes|str)->bytes:
         """get the pedersen proof alone and return the 32 bytes hash"""
         return pedersen_proof_to_hash(pedersen_proof)
