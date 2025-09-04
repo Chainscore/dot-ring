1c5d1180a14edce3d8c9d7ad2e4981b5bd599aa2
konasiva7 <sivakona@chainscore.finance>
2025-09-04 13:18:38 +0530

    updated ring proof tests

--- tests/test_ring_proof.py
+++ tests/test_ring_proof.py
@@ -10,21 +10,23 @@ def test_ring_proof():
     file_path = os.path.join(HERE, "ark-vrf/bandersnatch_ed_sha512_ell2_ring.json")
     with open(file_path, 'r') as f:
         data = json.load(f)
-    for index in range(len(data)-6):
+    for index in range(len(data)):
         if index < 0 or index >= len(data):
             raise IndexError("Index out of range")
         item = data[index]
         blinding = item['blinding']
+        print("blinding :", blinding)
         s_k =item['sk']
-        p_k = item['pk']
         alpha = item['alpha']
         ad = item['ad']
         B_keys_ring = bytes.fromhex(item['ring_pks'])
         B_keys=[B_keys_ring[32*i:32*(i+1)] for i in range(len(B_keys_ring)//32)]
-        RVRF=RingVrf()
+        RVRF = RingVrf()
+        p_k = RVRF.get_public_key(s_k)
         ring_root = RVRF.construct_ring_root(B_keys, False)
         ring_vrf_proof = RVRF.ring_vrf_proof(alpha, ad,s_k,p_k,B_keys,False)
         ring_proof_sign=RVRF.generate_bls_signature(blinding, p_k,B_keys, False)
+        assert p_k.hex()==item['pk'], "Invalid Public Key"
         assert ring_root.hex()==item['ring_pks_com'], "Invalid Ring Root"
         assert ring_proof_sign.hex()==item['ring_proof'], "Unexpected Ring Proof"
         rltn_to_proove=ring_vrf_proof[32:64]
