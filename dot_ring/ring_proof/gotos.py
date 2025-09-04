39f791ba364fa78e7c0da159a0444a1abdcea406
konasiva7 <sivakona@chainscore.finance>
2025-09-04 13:58:50 +0530

    updated logic to handle pk_ring

--- dot_ring/ring_proof/gotos.py
+++ dot_ring/ring_proof/gotos.py
@@ -30,11 +30,19 @@ from dot_ring.vrf.pedersen.pedersen import PedersenVRF
 # from dot_ring.vrf.pedersen.pedersen import PedersenVRF
 
 # def generate_bls_signature(secret_t,producer_key, keys: List[BandersnatchPublic]):
-def generate_bls_signature(secret_t:bytes|str,producer_key:bytes|str, keys: List[Any], third_party_msm:bool)->bytes:
+def generate_bls_signature(secret_t:bytes|str,producer_key:bytes|str, keys: List[Any]|bytes|str, third_party_msm:bool)->bytes:
     """
     get the all the data needed and
     return the signature as an output
     """
+    if isinstance(keys, str):
+        B_keys_ring = bytes.fromhex(keys)
+        keys = [B_keys_ring[32 * i:32 * (i + 1)] for i in range(len(B_keys_ring) // 32)]
+
+    elif isinstance(keys, bytes):
+        B_keys_ring = keys
+        keys = [B_keys_ring[32 * i:32 * (i + 1)] for i in range(len(B_keys_ring) // 32)]
+
     kzg = KZG.default(use_third_party_commit=third_party_msm)
     # signature = func() #make the call or logic u want
     # return signature
@@ -103,10 +111,18 @@ def generate_bls_signature(secret_t:bytes|str,producer_key:bytes|str, keys: List
     return bytes.fromhex(proof_bs) #bytess string
 
 # def construct_ring_root(keys: List[BandersnatchPublic]):
-def construct_ring_root(keys: List[Any], third_party_msm:bool)->bytes:
+def construct_ring_root(keys: List[Any]|str|bytes, third_party_msm:bool)->bytes:
     """
     get the data needed and construct the rng root
     """
+    if isinstance(keys, str):
+        B_keys_ring = bytes.fromhex(keys)
+        keys = [B_keys_ring[32 * i:32 * (i + 1)] for i in range(len(B_keys_ring) // 32)]
+
+    elif isinstance(keys, bytes):
+        B_keys_ring = keys
+        keys = [B_keys_ring[32 * i:32 * (i + 1)] for i in range(len(B_keys_ring) // 32)]
+
     kzg = KZG.default(use_third_party_commit=third_party_msm)
     # ring_root= func() make the call ore logic u want
     #return ring_root
@@ -171,10 +187,11 @@ def verify_signature(message:bytes|str, ring_root:bytes|str, proof:bytes|str)->b
 
 
 # def ring_vrf_proof(alpha, add, blinding_factor, producer_key, keys:List[BandersnatchPublic]):
-def ring_vrf_proof(alpha:bytes|str, add:bytes|str, secret_key:bytes|str, producer_key:bytes|str, keys:List[Any], third_party_msm:bool)->bytes:
+def ring_vrf_proof(alpha:bytes|str, add:bytes|str, secret_key:bytes|str, producer_key:bytes|str, keys:List[Any]|str|bytes, third_party_msm:bool)->bytes:
     """get the args u want and generate the
     ring_vrf_proof (pedersen vrf proof + ring_proof ) \
     which of length 784 bytes"""
+
     kzg = KZG.default(use_third_party_commit=third_party_msm)
 
     if not isinstance(alpha, bytes):
@@ -190,8 +207,21 @@ def ring_vrf_proof(alpha:bytes|str, add:bytes|str, secret_key:bytes|str, produce
     #pedersen_proof=get the pedersen proof
     vrf = PedersenVRF(Bandersnatch_TE_Curve, BandersnatchPoint)
     pedersen_proof, blinding_factor = vrf.proof(alpha,secret_key,add, True)
+
     #ring_proof= get the ring_signature
-    ring_proof= generate_bls_signature(blinding_factor,producer_key, keys, third_party_msm)
+    if isinstance(keys, list):
+        ring_proof= generate_bls_signature(blinding_factor,producer_key, keys, third_party_msm)
+
+    elif isinstance(keys, str):
+        B_keys_ring = bytes.fromhex(keys)
+        B_keys = [B_keys_ring[32 * i:32 * (i + 1)] for i in range(len(B_keys_ring) // 32)]
+        ring_proof = generate_bls_signature(blinding_factor, producer_key, B_keys, third_party_msm)
+
+    elif isinstance(keys, bytes):
+        B_keys_ring = keys
+        B_keys = [B_keys_ring[32 * i:32 * (i + 1)] for i in range(len(B_keys_ring) // 32)]
+        ring_proof = generate_bls_signature(blinding_factor, producer_key, B_keys, third_party_msm)
+
     if not isinstance(ring_proof, bytes):
         ring_proof=bytes.fromhex(ring_proof)
     rvrf_proof= pedersen_proof +ring_proof
@@ -232,7 +262,10 @@ def ring_vrf_proof_verify(context:bytes|str, ring_root:bytes|str, proof:bytes|st
     input_point = BandersnatchPoint.encode_to_curve(alpha)
 
     #is pedersen proof valid
+    pedersen_verify=time.time()
     p_proof_valid= vrf.verify(input_point, context , pedersen_proof)
+    pedersen_end=time.time()
+    print("Pedersen alone:", pedersen_end-pedersen_verify)
     rel_to_proove=BandersnatchPoint.string_to_point(pedersen_proof[32:64])
     #Extract and verify the Ring proof
     ring_proof=proof[192:]
--- dot_ring/vrf/ring/ring_vrf.py
+++ dot_ring/vrf/ring/ring_vrf.py
@@ -7,7 +7,7 @@ from dot_ring.ring_proof.gotos import*
 class RingVrf:
 
     @staticmethod
-    def generate_bls_signature(blinding: bytes|str,producer_key:bytes|str, keys: List[Any], third_party_msm:bool=False)->bytes:
+    def generate_bls_signature(blinding: bytes|str,producer_key:bytes|str, keys: List[Any]|str|bytes, third_party_msm:bool=False)->bytes:
         """
             get the all the data needed and
             return the signature as an output"
@@ -15,7 +15,7 @@ class RingVrf:
         return generate_bls_signature(blinding, producer_key, keys, third_party_msm)
 
     @staticmethod
-    def construct_ring_root(keys: List[Any], third_party_msm:bool=False)->bytes:
+    def construct_ring_root(keys: List[Any]|str|bytes, third_party_msm:bool=False)->bytes:
         """
         get the data needed and construct the rng root
         """
@@ -29,11 +29,11 @@ class RingVrf:
         return verify_signature(message,ring_root, ring_signature)
 
     @staticmethod
-    def ring_vrf_proof(alpha:bytes|str, add:bytes|str, secret_key:bytes|str, producer_key:bytes|str, keys:List[Any], third_party_msm:bool=False)->bytes:
+    def ring_vrf_proof(alpha:bytes|str, add:bytes|str, secret_key:bytes|str, producer_key:bytes|str, keys:List[Any]|str|bytes, third_party_msm:bool=False)->bytes:
         """get the args u want and generate the
         ring_vrf_proof (pedersen vrf proof + ring_proof ) \
         which of length 784 bytes"""
-        return ring_vrf_proof(alpha, add, secret_key,producer_key, keys, third_party_msm)
+        return ring_vrf_proof(alpha, add, secret_key, producer_key, keys, third_party_msm)
 
     @staticmethod
     def ring_vrf_proof_verify(ad_data:bytes|str, ring_root:bytes|str, signature:bytes|str, message:bytes|str=b"")->bool:
@@ -45,6 +45,11 @@ class RingVrf:
     def pedersen_proof_to_hash(pedersen_proof:bytes|str)->bytes:
         """get the pedersen proof alone and return the 32 bytes hash"""
         return pedersen_proof_to_hash(pedersen_proof)
+
+    @staticmethod
+    def get_public_key(secret_key:bytes|str)->bytes:
+        """Take the Secret_Key and return Public Key"""
+        return get_public_key(secret_key)
 
     @staticmethod
     def get_public_key(secret_key:bytes|str)->bytes:
