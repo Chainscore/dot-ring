88b3f1c46335ac79954fb2496c394d4eae609b53
konasiva7 <sivakona@chainscore.finance>
2025-09-19 18:15:36 +0530

    added tests for all variant h2c wrt rfc

--- tests/test_p256_ssw_e2c_nu.py
+++ tests/test_p256_ssw_e2c_nu.py
@@ -1,10 +1,10 @@
 
-from dot_ring.curve.specs.ed448 import Ed448_TE_Curve, Ed448Point
+from dot_ring.curve.specs.p256 import (P256_SW_Curve, P256Point)
 
 
 def test_sswu_hash2_curve():
     import json
-    json_path = "/home/siva/PycharmProjects/DT/dot_ring/tests/vectors/ed448_ro.json"
+    json_path = "/home/siva/PycharmProjects/DT/dot_ring/tests/vectors/p256_nu.json"
     # Load JSON
     with open(json_path, "r") as f:
         data = json.load(f)
@@ -12,16 +12,13 @@ def test_sswu_hash2_curve():
 
     for t in RFC_TEST_VECTORS:
         msg_bytes = t["msg"].encode("utf-8")
-        out = Ed448Point.encode_to_curve(msg_bytes, b"", True)
-        u0_bytes = out["u"][0].to_bytes(56, 'big').hex()
-        u1_bytes = out["u"][1].to_bytes(56, 'big').hex()
-        Px_bytes = out["P"][0].to_bytes(56, 'big').hex()
-        Py_bytes = out["P"][1].to_bytes(56, 'big').hex()
+        out = P256Point.encode_to_curve(msg_bytes, b"", True)
+        u0_bytes = out["u"][0].to_bytes(32, 'big').hex()
+        Px_bytes = out["P"][0].to_bytes(32, 'big').hex()
+        Py_bytes = out["P"][1].to_bytes(32, 'big').hex()
         print("msg:", repr(t["msg"]))
         print(" computed u[0] =", u0_bytes)
         print(" expected u[0] =", t["u"][0])
-        print(" computed u[1] =", u1_bytes)
-        print(" expected u[1] =", t["u"][1])
         print(" computed P.x =", Px_bytes)
         print(" expected P.x =", t["P"]["x"])
         print(" computed P.y =", Py_bytes)
