00776971b36b219e6e8ada1eccf58bf4bd14d861
konasiva7 <sivakona@chainscore.finance>
2025-09-30 00:41:23 +0530

    updated curve25519 nu_varinat test

--- tests/test_h2c_suites/test_curve25519_e2c_ell2_nu.py
+++ tests/test_h2c_suites/test_curve25519_e2c_ell2_nu.py
@@ -1,5 +1,6 @@
 import os
-from dot_ring.curve.specs.curve25519 import (Curve25519_MG_Curve, Curve25519Point)
+from dot_ring.curve.specs.curve25519 import nu_variant
+from dot_ring.curve.e2c import E2C_Variant
 
 
 def test_sswu_hash2_curve():
@@ -10,9 +11,13 @@ def test_sswu_hash2_curve():
     with open(json_path, "r") as f:
         data = json.load(f)
     RFC_TEST_VECTORS = data["vectors"]
-
-    for i,t in enumerate(RFC_TEST_VECTORS, start=1):
+    
+    # Simple one-liner to get point class with NU variant 
+    Curve25519Point= nu_variant(E2C_Variant.ELL2_NU)
+    
+    for i, t in enumerate(RFC_TEST_VECTORS, start=1):
         msg_bytes = t["msg"].encode("utf-8")
+        # Use the NU point class
         out = Curve25519Point.encode_to_curve(msg_bytes, b"", True)
         u0_bytes = out["u"][0].to_bytes(32, 'big').hex()
         Px_bytes = out["P"][0].to_bytes(32, 'big').hex()
@@ -20,4 +25,4 @@ def test_sswu_hash2_curve():
         assert u0_bytes == t["u"][0]
         assert Px_bytes == t["P"]["x"]
         assert Py_bytes == t["P"]["y"]
-        print(f"✅ Testcase {i + 1}")
+        print(f" Testcase {i + 1}")
