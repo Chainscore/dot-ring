05021e7860ef9ad0954729f19494a2448aa704ff
konasiva7 <sivakona@chainscore.finance>
2025-09-30 00:57:49 +0530

    updated enums

--- dot_ring/curve/e2c.py
+++ dot_ring/curve/e2c.py
@@ -3,5 +3,7 @@ from enum import Enum
 
 class E2C_Variant(Enum):
     TAI = "TryAndIncrement"
-    ELL2= "Hash2Suite_Elligator2_NU_" #Replace with NU for NU way of hash 2 curve
-    SSWU = "Simple_SWU_NU_" #Replace with NU for NU way of hash 2 curve
+    ELL2= "Hash2Suite_Elligator2_RO_" #Replace with NU for NU way of hash 2 curve
+    SSWU= "Simple_SWU_RO_" #Replace with NU for NU way of hash 2 curve
+    ELL2_NU="Hash2Suite_Elligator2_NU_"
+    SSWU_NU="Simple_SWU_NU_"
