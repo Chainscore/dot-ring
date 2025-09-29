c6f964bea969dc15e9d2709dd95b87100643ca80
konasiva7 <sivakona@chainscore.finance>
2025-09-29 19:34:43 +0530

    added tests for p384 curve's ietf & pedersen vrf

---
+++ tests/test_extra_vrfs/test_p384_sswu_ietf.py
@@ -0,0 +1,28 @@
+import json
+import os
+import pytest
+from dot_ring.curve.specs.p384 import P384_SW_Curve, P384Point
+from dot_ring.vrf.ietf.ietf import IETF_VRF
+HERE = os.path.dirname(__file__)
+# @pytest.mark.skipif("RUNALL" not in os.environ, reason="takes too long")
+def test_prove_bandersnatch_ed_sha512_ell2_ietf():
+    # Construct the relative path to the data folder
+    data_dir = os.path.join(HERE, 'vectors')
+    data_dir = os.path.abspath(data_dir)  # normalize the path
+    limit = 10000
+    for i, file in enumerate(os.listdir(data_dir)):
+        if i >= limit:
+            break
+        if not file.startswith("p384_base_vectors"):
+            continue
+        with open(os.path.join(data_dir, file), "r") as f:
+            data = json.loads(f.read())
+            for i, vector in enumerate(data):
+                secret_scalar = vector["sk"]
+                vrf = IETF_VRF(P384_SW_Curve, P384Point)
+                input_point=P384Point.encode_to_curve(vector['alpha']).point_to_string()
+                proof = vrf.proof(vector["alpha"],secret_scalar,vector["ad"])
+                public_key=P384Point.string_to_point(vrf.get_public_key(vector['sk']))
+                verified=vrf.verify(public_key, P384Point.string_to_point(input_point), vector["ad"], proof)
+                assert verified, "Proof Validation Failed"
+                print(f"✅ Testcase {i + 1} of {file}")
