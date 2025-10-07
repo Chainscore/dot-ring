7dbf135bf901153e195e04d07622f49fb3353418
konasiva7 <sivakona@chainscore.finance>
2025-10-07 13:35:09 +0530

    verified and added curve25516,curve448, ed448 curves ietf tests

--- tests/test_vrfs/test_ietf/test_curve25519_ell2_ietf.py
+++ tests/test_vrfs/test_ietf/test_curve25519_ell2_ietf.py
@@ -3,8 +3,7 @@ import os
 import pytest
 HERE = os.path.dirname(__file__)
 
-from dot_ring.curve.specs.curve448 import Curve448_MG_Curve,Curve448Point
-
+from dot_ring.curve.specs.curve25519 import Curve25519_MG_Curve,Curve25519Point
 from dot_ring.vrf.ietf.ietf import IETF_VRF
 
 # @pytest.mark.skipif("RUNALL" not in os.environ, reason="takes too long")
@@ -17,17 +16,16 @@ def test_prove_bandersnatch_ed_sha512_ell2_ietf():
     for i, file in enumerate(os.listdir(data_dir)):
         if i >= limit:
             break
-        if not file.startswith("curve448_base_vectors"):
+        if not file.startswith("curve25516_base_vectors"):
             continue
         with open(os.path.join(data_dir, file), "r") as f:
             data = json.loads(f.read())
             for i, vector in enumerate(data):
                 secret_scalar = vector["sk"]
-                vrf = IETF_VRF(Curve448_MG_Curve, Curve448Point)
-                input_point=Curve448Point.encode_to_curve(vector['alpha'])
-                input_point=input_point.point_to_string()
-                public_key=Curve448Point.string_to_point(vrf.get_public_key(vector['sk']))
+                vrf = IETF_VRF(Curve25519_MG_Curve, Curve25519Point)
+                input_point=Curve25519Point.encode_to_curve(vector['alpha'])
+                public_key=Curve25519Point.string_to_point(vrf.get_public_key(vector['sk']))
                 proof = vrf.proof(vector["alpha"],secret_scalar,vector["ad"])
-                verified=vrf.verify(public_key,Curve448Point.string_to_point(input_point), vector["ad"], proof)
-                assert verified, "Proof Validation Failed"
+                verified=vrf.verify(public_key,input_point, vector["ad"], proof)
+                assert verified, "Proof Verification Failed"
                 print(f"✅ Testcase {i + 1} of {file}")
