6443c6002cbd421c88bb26d1c0990d618863fc6c
konasiva7 <sivakona@chainscore.finance>
2025-10-08 19:10:17 +0530

    feat:verified and added tests for ed25519 ietf and pedersen vrfs with ell2_h2c variant

---
+++ tests/test_vrfs/test_ietf/test_ed25519_ell2_ietf.py
@@ -0,0 +1,31 @@
+import json
+import os
+import pytest
+HERE = os.path.dirname(__file__)
+
+from dot_ring.curve.specs.ed25519 import Ed25519_TE_Curve, Ed25519Point
+from dot_ring.vrf.ietf.ietf import IETF_VRF
+
+# @pytest.mark.skipif("RUNALL" not in os.environ, reason="takes too long")
+def test_prove_bandersnatch_ed_sha512_ell2_ietf():
+    # Get the directory of the current test file
+    # Construct the relative path to the data folder
+    data_dir = os.path.join(HERE, "./..", 'vectors')
+    data_dir = os.path.abspath(data_dir)  # normalize the path
+    limit = 10000
+    for i, file in enumerate(os.listdir(data_dir)):
+        if i >= limit:
+            break
+        if not file.startswith("ed25519_base_vectors"):
+            continue
+        with open(os.path.join(data_dir, file), "r") as f:
+            data = json.loads(f.read())
+            for i, vector in enumerate(data):
+                secret_scalar = vector['sk']
+                vrf = IETF_VRF(Ed25519_TE_Curve, Ed25519Point)
+                input_point=Ed25519Point.encode_to_curve(vector['alpha'])
+                public_key=Ed25519Point.string_to_point(vrf.get_public_key(secret_scalar))
+                proof = vrf.proof(vector["alpha"],secret_scalar,vector["ad"])
+                verified=vrf.verify(public_key,input_point, vector["ad"], proof)
+                assert verified, "Proof Verification Failed"
+                print(f"✅ Testcase {i + 1} of {file}")
